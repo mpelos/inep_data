06086461a4af7465331cae883a21d04c7abd563c
Marcelo Munhoz Pélos <mpelos@gmail.com>
2013-07-30 14:43:10 -0300

    Cria models para o MongoDB.

---
+++ models.py
@@ -0,0 +1,21 @@
+from mongoengine import *
+
+connect("inep_data_development")
+
+class School(Document):
+    code = IntField(required=True)
+    city_code = IntField()
+    state_code = IntField()
+    name = StringField()
+    scores = ListField(default = [0 for n in xrange(0,20)])
+
+class City(Document):
+    code = IntField(required=True)
+    state_code = IntField()
+    name = StringField()
+    scores = ListField(default = [0 for n in xrange(0,20)])
+
+class State(Document):
+    code = IntField(required=True)
+    acronym = StringField();
+    scores = ListField(default = [0 for n in xrange(0,20)])
