b82d44ce57882afff55851385609b73b3bff76dd
Marcelo Munhoz Pélos <mpelos@gmail.com>
2013-07-30 15:47:26 -0300

    Cria select com todas as escolas.

--- inep_data.py
+++ inep_data.py
@@ -1,9 +1,12 @@
 from flask import Flask, render_template
+from models import School
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    schools = School.objects.where("this.name != null").order_by("name")
+    return render_template('index.html', schools = schools)
 
 if __name__ == '__main__':
     app.run()
