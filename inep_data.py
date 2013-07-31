fab2ad70175df26c9b8bb11e7c9cc8a415996bdf
Marcelo Munhoz Pélos <mpelos@gmail.com>
2013-07-31 12:16:17 -0300

    Cria api JSON para escolas.

--- inep_data.py
+++ inep_data.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from models import School
+from flask import Flask, render_template, jsonify, request
+from models import School, City
 
 app = Flask(__name__)
 
@@ -7,6 +7,30 @@ app = Flask(__name__)
 def index():
     schools = School.objects.where("this.name != null").order_by("name")
     return render_template('index.html', schools = schools)
+
+@app.route("/schools/<int:code>")
+def school_show(code):
+    try:
+        school = School.objects.get(code = code)
+        city = City.objects.get(code = school.city_code)
+    except:
+        return "Not Found", 404
+
+    json = {
+        "school": {
+            "name": school.name,
+            "scores": school.scores,
+            "relative_scores": [(n * 100) / sum(school.scores) for n in school.scores]
+        },
+
+        "city": {
+            "name": city.name,
+            "scores": city.scores,
+            "relative_scores": [(n * 100) / sum(city.scores) for n in city.scores]
+        }
+    }
+
+    return jsonify(json)
 
 if __name__ == '__main__':
     app.run()
